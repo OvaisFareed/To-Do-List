76f66d9a5ead2a11c2affbbfcb61bdb002d8d8d2
OvaisFareed <movaissiddiqui16@gmail.com>
2015-05-03 03:08:39 +0500

    Modify To-do-list with firebase

--- To-Do-List/public_html/myJs.js
+++ To-Do-List/public_html/myJs.js
@@ -2,19 +2,26 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-var app = angular.module("myModule",[]);
+var app = angular.module("myModule",["ngMaterial","firebase"]);
 
-app.controller("myCtrl",function($scope){
+app.controller("myCtrl",function($scope,$firebaseObject){
+    var ref = new Firebase("https://myto-dolist.firebaseio.com/");
+
+    var syncObject  = $firebaseObject(ref);
+    syncObject.$bindTo($scope, "user");
+    
    $scope.task = "";
    $scope.tasks = [];
    $scope.addTask = function(task){
-          $scope.task = "";
-       $scope.tasks.push(task);
+   $scope.task = "";
+   $scope.tasks.push(task);
    };
   
    $scope.deleteTask = function(task){
   var index = $scope.tasks.indexOf(task);
-  $scope.tasks.splice(index, 1);     
+  $scope.tasks.splice(index, 1);
+  if($scope.tasks.length === 0)
+      $scope.doneTasks.pop();
 };
 
 $scope.doneTasks = [];
